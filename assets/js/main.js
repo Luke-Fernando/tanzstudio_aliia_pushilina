f733186c681f1eeb6ce2dd8a1351dfd076099b20
Luke-Fernando <isanjaya139@gmail.com>
2023-09-01 19:43:31 +0530

    navbar added

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+function animateHamburger(condition) {
+  const hamburgerMiddleBar = document.getElementById("navbar-hamburger-icon-middle-bar");
+  if (condition) {
+    hamburgerMiddleBar.classList.add("hamburger-clicked");
+  } else {
+    hamburgerMiddleBar.classList.remove("hamburger-clicked");
+  }
+}
+function showSideMenu(condition) {
+  const sideMenu = document.getElementById("navbar-side-menu");
+  const sideMenuBackdrop = document.getElementById("navbar-side-menu-backdrop");
+
+  if (condition) {
+    sideMenu.classList.add("side-menu-open");
+    sideMenuBackdrop.classList.add("side-menu-backdrop-visible");
+  } else {
+    sideMenu.classList.remove("side-menu-open");
+    sideMenuBackdrop.classList.remove("side-menu-backdrop-visible");
+  }
+}
+function blockScroll(condition) {
+  let rootEle = document.documentElement;
+  if (condition) {
+    rootEle.classList.add("scroll-block");
+  } else {
+    rootEle.classList.remove("scroll-block");
+  }
+}
+function openSideMenu() {
+  const body = document.getElementById("body");
+  const hamburgerBtn = document.getElementById("navbar-hamburger-btn");
+  const sideMenuBackdrop = document.getElementById("navbar-side-menu-backdrop");
+  let sideMenuStatus = false;
+
+  hamburgerBtn.addEventListener("click", function () {
+    sideMenuStatus = !sideMenuStatus;
+    animateHamburger(sideMenuStatus);
+    showSideMenu(sideMenuStatus);
+    blockScroll(sideMenuStatus);
+  });
+  sideMenuBackdrop.addEventListener("click", function () {
+    sideMenuStatus = !sideMenuStatus;
+    animateHamburger(sideMenuStatus);
+    showSideMenu(sideMenuStatus);
+    blockScroll(sideMenuStatus);
+  });
+}
+
+openSideMenu();
+
+document.addEventListener("scroll", function (e) {
+  e.preventDefault();
+});
