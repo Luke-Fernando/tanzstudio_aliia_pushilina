4525726e814d4e48879035c0f075e697c3c4ddb5
Luke-Fernando <isanjaya139@gmail.com>
2023-09-02 13:56:31 +0530

    made active status in navbar links change with the user scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,6 +48,67 @@ function openSideMenu() {
 
 openSideMenu();
 
-document.addEventListener("scroll", function (e) {
-  e.preventDefault();
-});
+function toggleNavbarPosition() {
+  const homeSec = document.getElementById("home");
+  const navBar = document.getElementById("navbar");
+  const options = {
+    threshold: 0,
+  };
+
+  let navObserver = new IntersectionObserver((entries) => {
+    console.log(entries);
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log("visible");
+        navBar.classList.add("navbar-absolute");
+        navBar.classList.remove("navbar-fixed");
+        navBar.classList.remove("navbar-purple");
+      } else {
+        console.log("not visible");
+        navBar.classList.remove("navbar-absolute");
+        navBar.classList.add("navbar-fixed");
+        navBar.classList.add("navbar-purple");
+      }
+    });
+  }, options);
+
+  navObserver.observe(homeSec);
+}
+toggleNavbarPosition();
+// active section monitoring
+function placeActiveNav(selector, activeLink) {
+  let element = document.querySelector(selector);
+  const elementRect = element.getBoundingClientRect();
+  let topPosition = elementRect.top;
+  let navLinks = document.querySelectorAll("[data-nav-link]");
+
+  console.log(topPosition);
+  if (topPosition == 0) {
+    navLinks.forEach((link) => {
+      link.classList.remove("active-nav-link");
+    });
+    activeLink.classList.add("active-nav-link");
+  }
+}
+
+function monitorActiveSection(sectionId) {
+  let element = document.querySelector(sectionId);
+  let activeLink = document.querySelector(`[href='${sectionId}']`);
+  function handle() {
+    placeActiveNav(sectionId, activeLink);
+  }
+  let activeSectionObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        window.addEventListener("scroll", handle, true);
+      } else {
+        window.removeEventListener("scroll", handle, true);
+        activeLink.classList.remove("active-nav-link");
+      }
+    });
+  });
+
+  activeSectionObserver.observe(element);
+}
+monitorActiveSection("#home");
+// active section monitoring
